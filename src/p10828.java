adf45753d570516a829a0fc51cfcc0cbbe0db348
yuhyun1 <yh7119@gmai.com>
2023-04-19 18:19:46 +0900

    클래스명 변경

--- src/p10828.java
+++ src/p10828.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 import java.util.Stack;
 
-public class Main {
+public class p10828 {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
