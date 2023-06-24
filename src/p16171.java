b46760ebbad37ff79517b45c7349939f6fd69a7c
yuhyun1 <yh7119@gmail.com>
2023-06-24 23:58:21 +0900

    백준 1문제

---
+++ src/p16171.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class p16171 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+        String s = "";
+        String find = sc.nextLine();
+        for (int i = 0; i < str.length(); i++) {
+            if (!(str.charAt(i) - '0' >= 0 && str.charAt(i) - '0' <= 9))
+                s += str.charAt(i);
+        }
+        System.out.println(s.contains(find) ? 1 : 0);
+    }
+}
