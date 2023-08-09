45885215eb396444869574262d99414fa1cdf989
yuhyun1 <yh7119@gmail.com>
2023-08-09 15:53:49 +0900

    백준 1문제 (자료 구조, 브루트포스, 해시 집합과 맵, 백트래킹)

---
+++ src/p5568.java
@@ -0,0 +1,47 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashSet;
+
+public class p5568 {
+    static int n;
+    static int k;
+    static int[] arr;
+    static int[] v;
+    static boolean[] visit;
+    static HashSet<Integer> set = new HashSet<>();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        n = Integer.parseInt(br.readLine());
+        k = Integer.parseInt(br.readLine());
+        arr = new int[n];
+        v = new int[k];
+        visit = new boolean[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+
+        dfs(0);
+        System.out.println(set.size());
+    }
+
+    public static void dfs(int depth) {
+        if (depth == k) {
+            String sum = "";
+            for (int i = 0; i < v.length; i++) {
+                sum += v[i];
+            }
+            set.add(Integer.parseInt(sum));
+            return;
+        }
+        for (int i = 0; i < n; i++) {
+            if (!visit[i]) {
+                visit[i] = true;
+                v[depth] = arr[i];
+                dfs(depth + 1);
+                visit[i] = false;
+            }
+        }
+    }
+}
