5ac47e73ab80e64bdee45395877a0188a6c46dbe
yuhyun1 <yh7119@gmail.com>
2023-09-17 00:42:55 +0900

    백준 1문제 (수학, 구현, 재귀)

---
+++ src/p10870.java
@@ -0,0 +1,19 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class p10870 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        System.out.print(fibonacci(n));
+    }
+
+    public static int fibonacci(int n) {
+        if (n == 0) return 0;
+        if (n == 1) return 1;
+
+        return fibonacci(n - 1) + fibonacci(n - 2);
+    }
+}
