4f783cb404b49d10d4ecafd9a60aa662c846ad34
yuhyun1 <yh7119@gmail.com>
2023-10-16 23:16:32 +0900

    백준 1문제 (문자열)

--- src/p16171.java
+++ src/p16171.java
@@ -1,11 +1,13 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class p16171 {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        String str = sc.nextLine();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str = br.readLine();
         String s = "";
-        String find = sc.nextLine();
+        String find = br.readLine();
         for (int i = 0; i < str.length(); i++) {
             if (!(str.charAt(i) - '0' >= 0 && str.charAt(i) - '0' <= 9))
                 s += str.charAt(i);
