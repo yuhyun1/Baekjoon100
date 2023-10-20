83005ea934e7d34f0ce3472d961afbb69bfdbd77
yuhyun1 <yh7119@gmail.com>
2023-10-21 00:24:50 +0900

    백준 1문제 (누적 합, 브루트포스 알고리즘, 투 포인터)

--- src/p2003Better.java
+++ src/p2003Better.java
@@ -7,37 +7,29 @@ public class p2003Better {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-
         int n = Integer.parseInt(st.nextToken());
         int m = Integer.parseInt(st.nextToken());
 
-        st = new StringTokenizer(br.readLine());
         int[] arr = new int[n];
-
-        for(int i = 0; i < n; i++) {
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
             arr[i] = Integer.parseInt(st.nextToken());
         }
 
-        System.out.println(twoPointer(arr, m));
-    }
 
-    static int twoPointer(int[] arr, int m) {
-        int count = 0;
-        int startPoint = 0, endPoint = 0;
-        int len = arr.length;
-        int sum = 0;
-
+        int s = 0, e = 0, sum = 0, cnt = 0;
         while(true) {
-            // M 보다 sum이 크면 값을 줄여서 M을 맞춰야 하므로 현재 startIndex의 값을 빼고 한 칸 앞으로 이동해야 한다.
-            if(sum >= m) {
-                sum -= arr[startPoint++];
-
-            } else if(endPoint >= len) break;
-            else { // M 보다 sum이 작으면 값을 늘려서 M을 맞춰야 하므로 현재 endIndex를 한 칸 앞으로 이동시키고 그인덱스의 원소 값을 더해줘야 한다.
-                sum += arr[endPoint++];
+            if (sum >= m) {
+                sum -= arr[s++];
+            } else if (e == n) {
+                break;
+            } else {
+                sum += arr[e++];
             }
-            if(sum == m) count++;
+
+            if (sum == m) cnt++;
+
         }
-        return count;
+        System.out.println(cnt);
     }
 }
