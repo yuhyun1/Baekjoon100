1b17241cd527f33e69ff318b2d3fd0af4c1172d1
yuhyun1 <yh7119@gmail.com>
2023-11-01 16:42:13 +0900

    백준 1문제 (구현, 정렬)

--- src/p2750.java
+++ src/p2750.java
@@ -1,13 +1,15 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Arrays;
-import java.util.Scanner;
 
 public class p2750 {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
         int[] arr = new int[n];
         for (int i = 0; i < n; i++) {
-            arr[i] = sc.nextInt();
+            arr[i] = Integer.parseInt(br.readLine());
         }
         Arrays.sort(arr);
         for (int i = 0; i < arr.length; i++) {
