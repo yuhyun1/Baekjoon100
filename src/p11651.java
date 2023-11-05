14308f4701cc779bc882f0011eef90a212478c20
yuhyun1 <yh7119@gmail.com>
2023-11-05 14:25:16 +0900

    백준 1문제 (정렬)

--- src/p11651.java
+++ src/p11651.java
@@ -28,7 +28,7 @@ public class p11651 {
 
         StringBuilder sb = new StringBuilder();
         for(int i = 0; i < n ; i++) {
-            sb.append(arr[i][1] + " " + arr[i][0]).append('\n');
+            sb.append(arr[i][1]).append(" ").append(arr[i][0]).append('\n');
         }
         System.out.println(sb);
     }
